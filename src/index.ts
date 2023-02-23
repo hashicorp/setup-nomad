013ef87de1c12f8a371da375417e3016c85ae6ec
Kerim Satirli <k.satirli@gmail.com>
2023-02-23 12:48:32 +0100

    adds Action logic

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as core from "@actions/core";
+import * as hc from "@hashicorp/github-actions-core";
+
+async function main() {
+  const version = core.getInput("version");
+  if (version == null) {
+    core.setFailed("Required field 'version' not set");
+    return;
+  }
+  try {
+    await hc.getHashicorpRelease("nomad", version);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  }
+}
+
+main();
